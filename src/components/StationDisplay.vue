c1e95b1e1f435879ef0a8e8a3d61256c706b7020
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2024-04-28 21:52:39 +0200

    Revert "more coding"
    This reverts commit 9b73842ab5a458735a3f0d0b630e62921af262b6.

--- src/components/StationDisplay.vue
+++ src/components/StationDisplay.vue
@@ -1,31 +1,14 @@
-<script lang="ts" setup>
-import { computed, ref } from "vue";
-import { type Connection, getTimetable } from "@/scripts/searchAPI";
-import SingleLine from "@/components/SingleLine.vue";
+<script setup lang="ts">
+import { computed } from 'vue'
 
-const props = defineProps(["station"]);
+const props = defineProps(['station'])
 const station = computed(() => props.station as string);
-
-const connections = ref([] as Connection[]);
-
-getTimetable(station.value).then(
-  (response) => (connections.value = response.connections),
-);
 </script>
 
 <template>
-  <div class="container">
-    <SingleLine :labels="true"></SingleLine>
-    <SingleLine
-      v-for="connection in connections"
-      :connection="connection"
-    ></SingleLine>
-  </div>
+  <h1>Welcome to {{ station }}</h1>
 </template>
 
 <style scoped>
-.container {
-  height: 100vh;
-  width: 100vw;
-}
+
 </style>
